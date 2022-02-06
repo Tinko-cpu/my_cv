0d44e048f21de061164725580f62090bc541e78e
Tinko-cpu <69428235+Tinko-cpu@users.noreply.github.com>
2022-02-06 17:01:45 +0000

    add initial cv website

---
+++ index.js
@@ -0,0 +1,10 @@
+markWeight = 78;
+markHeight = 1.69;
+
+johnWeight = 92;
+johnHeight = 1.95;
+
+markBmi = markWeight / markHeight ** 2;
+johnBmi = johnWeight / johnHeight ** 2;
+
+console.log(markBmi, johnBmi)
